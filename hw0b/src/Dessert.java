fd3183ceef93aac70601266f28c980d808448fe0
luoyu <2509124889@qq.com>
2025-03-26 16:53:13 +0800

    hw0b

---
+++ hw0b/src/Dessert.java
@@ -0,0 +1,20 @@
+public class Dessert {
+    static int numDesserts;
+    int flavor;
+    int price;
+
+    Dessert(int flavor, int price){
+        this.flavor = flavor;
+        this.price = price;
+
+        numDesserts++;
+    }
+
+    public void printDessert(){
+        System.out.println(flavor + " " + price + " " +  numDesserts);
+    }
+
+    public static void main(String[] args){
+        System.out.println("I love dessert!");
+    }
+}
--- hw0b/src/ListExercises.java
+++ hw0b/src/ListExercises.java
@@ -1,29 +1,66 @@
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class ListExercises {
 
     /** Returns the total sum in a list of integers */
 	public static int sum(List<Integer> L) {
-        // TODO: Fill in this function.
-        return 0;
+        if(L == null || L.isEmpty()){
+            return 0;
+        }
+
+        int sum = 0;
+        for (Integer i : L) {
+            sum += i;
+        }
+
+        return sum;
     }
 
     /** Returns a list containing the even numbers of the given list */
     public static List<Integer> evens(List<Integer> L) {
-        // TODO: Fill in this function.
-        return null;
+        List<Integer> ans = new ArrayList<>();
+
+        for (Integer i : L) {
+            if(i % 2 == 0){
+                ans.add(i);
+            }
+        }
+
+        return ans;
     }
 
     /** Returns a list containing the common item of the two given lists */
     public static List<Integer> common(List<Integer> L1, List<Integer> L2) {
-        // TODO: Fill in this function.
-        return null;
+        List<Integer> ans = new ArrayList<>();
+
+        for (Integer i : L1) {
+            for (Integer integer : L2) {
+                if(Objects.equals(i, integer)){
+                    ans.add(i);
+                    break;
+                }
+            }
+        }
+
+        return ans;
     }
 
 
     /** Returns the number of occurrences of the given character in a list of strings. */
     public static int countOccurrencesOfC(List<String> words, char c) {
-        // TODO: Fill in this function.
-        return 0;
+        int ans = 0;
+
+        for (String word : words) {
+            char []chars = word.toCharArray();
+            for (char aChar : chars) {
+                if(aChar == c){
+                    ans++;
+                }
+            }
+        }
+
+        return ans;
     }
 }
