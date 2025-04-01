cb1bd525f1e7cf349ce456b421d0bad3cf36784a
luoyu <2509124889@qq.com>
2025-04-01 22:21:34 +0800

    lab3

--- lab03/src/adventure/MachineStage.java
+++ lab03/src/adventure/MachineStage.java
@@ -78,26 +78,12 @@ public class MachineStage implements AdventureStage {
     }
 
     public static int mysteryMax(int a, int b) {
-        int w = (b - a) >> 31;
-        int z = ~(b - a) >> 31;
-
-        int max = b & w | a & z;
+        int max = Math.max(a, b);
         return max;
     }
 
     public static int mysteryAdd(int a, int b) {
-        int x = a, y = b;
-        int xor, and, temp;
-        and = x & y;
-        xor = x ^ y;
-
-        while (and != 0) {
-            and <<= 1;
-            temp = xor ^ and;
-            and &= xor;
-            xor = temp;
-        }
-        return xor;
+        return b;
     }
 
     /**
